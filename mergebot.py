5065d276099e8fee653be824815d25fb5ca10f3e
Jason Kuster <jasonkuster@google.com>
2017-05-11 14:36:15 -0700

    Adds status piping throughout mergebot for use by frontend.
    Signed-off-by: Jason Kuster <jasonkuster@google.com>

--- mergebot.py
+++ mergebot.py
@@ -7,6 +7,9 @@ import glob
 import logging
 from multiprocessing import Pipe, Process
 import signal
+from threading import Thread
+from time import sleep
+
 import mergebot_poller
 import yaml
 
@@ -16,25 +19,27 @@ def poll_scm(config, pipe):
 
     Args:
         config: A dictionary of configuration to use for the poller.
+        pipe: Communication pipe for passing messages.
     """
     poller = mergebot_poller.create_poller(config, pipe)
     poller.poll()
 
 
 class MergerInfo(object):
-  def __init__(self, process, pipe, last_heartbeat):
-    self.process = process
-    self.pipe = pipe
-    self.last_heartbeat = last_heartbeat
+    def __init__(self, process, pipe, last_heartbeat):
+        self.process = process
+        self.pipe = pipe
+        self.last_heartbeat = last_heartbeat
 
 
 def main():
-    """Reads configs and kicks off pollers.
+    _, child_pipe = Pipe()
+    mb = MergeBot(child_pipe)
+    mb.start()
+    mb.join()
 
-    main reads the configs and then kicks off pollers per config file
-    successfully read in. It then waits for a keyboard interrupt as the
-    signal to shut down itself and its children.
-    """
+
+def get_logger():
     l = logging.getLogger('mergebot')
     log_fmt = '[%(levelname).1s-%(asctime)s %(filename)s:%(lineno)s] %(message)s'
     date_fmt = '%m/%d %H:%M:%S'
@@ -43,94 +48,71 @@ def main():
     h.setFormatter(f)
     l.setLevel(logging.INFO)
     l.addHandler(h)
-    l.info('Mergebot Manager Starting Up')
-    configs = []
-    l.info('Parsing Config Files')
-    for filename in glob.iglob('config/*.yaml'):
-        with open(filename) as cfg:
-            try:
-                l.info('Opening {}'.format(filename))
-                config = yaml.load(cfg)
-                l.info('{} Successfully Read'.format(filename))
-                configs.append(config)
-            except yaml.YAMLError as exc:
-                l.error('Error parsing file {}: {}.'.format(filename, exc))
-                l.error('Please fix and try again.')
-                return
+    return l
 
-    # Start up processes for each merger for which we have a config.
-    mergers = []
-    # Workaround for multiprocessing SIGINT problems per
-    # http://stackoverflow.com/questions/11312525 and the like. Children need to
-    # ignore SIGINT; parent should obey and clean up itself.
-    original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
-    for config in configs:
-        parent_pipe, child_pipe = Pipe()
-        p = Process(target=poll_scm, args=(config, child_pipe,))
-        mergers.append(MergerInfo(
-            process=p,
-            pipe=parent_pipe,
-            last_heartbeat=datetime.now()))
-        l.info('Starting poller for {}.'.format(config['name']))
-        p.start()
-    signal.signal(signal.SIGINT, original_sigint_handler)
 
-    # Watch heartbeats of mergers
-    try:
+class MergeBot(Thread):
+
+    def __init__(self, message_pipe):
+        self.message_pipe = message_pipe
+        self.l = get_logger()
+        self.l.info('Mergebot Manager Starting Up')
+        Thread.__init__(self)
+
+    def run(self):
+        """Reads configs and kicks off pollers.
+
+        start_pollers reads the configs and then kicks off pollers per config file
+        successfully read in. It then waits for a keyboard interrupt as the signal
+        to shut down itself and its children.
+        """
+        configs = self.parse_configs()
+        mergers = self.start_mergers(configs)
+
         while True:
+            if self.message_pipe.poll():
+                msg = self.message.recv()
+                if msg == 'terminate':
+                    break
             for merger in mergers:
-                msgs = []
                 while merger.pipe.poll():
-                    msgs.append(merger.pipe.recv())
-                for msg in msgs:
-                    # Check heartbeat, update last heard from, etc.
-                    # msgs will be FIFO, so update last_heartbeat as we go.
-                    if msg.startswith('hb: '):
-                        merger.last_heartbeat = datetime.strptime(
-                            msg[len('hb: '):],
-                            "%H:%M:%S-%Y-%m-%d")
-                    ts = (datetime.now() -
-                          merger.last_heartbeat).total_seconds()
-                    if ts > 60:
-                        # TODO(jasonkuster): Restart the process.
-                        # TODO(jasonkuster): How do we preserve queue?
-                        l.warn('houston we have a problem')
-                        # do something
-    except KeyboardInterrupt:
-        l.info('Caught KeyboardInterrupt; killing children and ending.')
+                    self.message_pipe.send(merger.pipe.recv())
+            sleep(1)
 
-    for merger in mergers:
-        merger.pipe.send('terminate')
+        for merger in mergers:
+            merger.pipe.send('terminate')
+        for merger in mergers:
+            merger.process.join()
+        self.l.info('Children killed, done.')
 
-    for merger in mergers:
-        merger.process.join()
+    def parse_configs(self):
+        configs = []
+        self.l.info('Parsing Config Files')
+        for filename in glob.iglob('config/*.yaml'):
+            with open(filename) as cfg:
+                try:
+                    self.l.info('Opening {}'.format(filename))
+                    config = yaml.load(cfg)
+                    self.l.info('{} Successfully Read'.format(filename))
+                    configs.append(config)
+                except yaml.YAMLError as exc:
+                    self.l.fatal(
+                        'Error parsing file {filename}: {exc}. Please fix and try '
+                        'again.'.format(filename=filename, exc=exc))
+        return configs
 
-    l.info('Children killed, done.')
-
-    # Workaround for multiprocessing SIGINT problems per
-    # http://stackoverflow.com/questions/11312525 and the like. Children need to
-    # ignore SIGINT; parent should obey and clean up itself.
-    # original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
-    # pool = Pool(len(configs))
-    # signal.signal(signal.SIGINT, original_sigint_handler)
-
-    # try:
-        # l.info('Starting Poller Pool')
-        # pool.map_async(poll_scm, configs)
-        # while True:
-        #     input('Mergebot running; press Ctrl + C to cancel.\n')
-    # except KeyboardInterrupt:
-        # l.info('Exiting.')
-        # pool.terminate()
-    # else:
-        # Generally this shouldn't happen - pollers should run forever.
-        # pool.close()
-        # pool.join()
-        # TODO(jasonkuster): We could get into a state where we're only polling
-        # one repo and all others have crashed. Once mergebot is functional,
-        # work on better poller management.
-        # l.error('Mergebot terminated early. All pollers must have crashed.')
-
+    def start_mergers(self, configs):
+        mergers = []
+        for config in configs:
+            parent_pipe, child_pipe = Pipe()
+            p = Process(target=poll_scm, args=(config, child_pipe,))
+            mergers.append(MergerInfo(
+                process=p,
+                pipe=parent_pipe,
+                last_heartbeat=datetime.now()))
+            self.l.info('Starting poller for {}.'.format(config['name']))
+            p.start()
+        return mergers
 
 if __name__ == '__main__':
     main()
